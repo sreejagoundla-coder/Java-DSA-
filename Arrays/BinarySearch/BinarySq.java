6e9ae0f6789d6f379e7f91439efeeacdc8cdaa61
sreeja <sreejagoundla@gmail.com>
2026-07-09 00:28:38 +0530

    Implement Binary Search

---
+++ Arrays/BinarySearch/BinarySq.java
@@ -0,0 +1,25 @@
+class BinarySq{
+    public static int BinarySq(int x) {
+        if (x == 0){ 
+            return 0;
+        }
+       int left = 1;
+         int right = x;
+          int ans = -1;
+          while(left <= right){
+           int mid = left + (right - left) / 2;
+           if(mid <= x / mid){
+               ans = mid;
+               left = mid + 1;
+           }else{
+                right = mid - 1;
+           }
+    }
+    return ans;
+}
+    public static void main(String[] args) {
+        int x = 10;
+        int result = BinarySq(x);
+        System.out.println(result);
+    }
+}
