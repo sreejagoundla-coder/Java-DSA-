0892fba79f194b6a0a0444e367d3d652ab224a0d
sreeja <sreejagoundla@gmail.com>
2026-07-12 00:08:34 +0530

    Added Rotate Sorted Array 2 problem

---
+++ Arrays/RotateSort2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class RotateSort2 {
+    public static void main(String[] args) {
+int[] nums = {2,2,2,0,1,2};
+int left=0;
+int right=nums.length-1;
+int ans=Integer.MAX_VALUE;
+while(left<=right){
+    int mid=left+(right-left)/2;
+    if(nums[left]==nums[mid] && nums[mid]==nums[right]){
+        ans = Math.min(ans, nums[left]);
+        left++;
+        right--;
+    }
+    else if(nums[left]<=nums[mid]){
+        ans=Math.min(ans,nums[left]);
+        left=mid+1;
+    }
+    else{
+        ans=Math.min(ans,nums[mid]);
+        right=mid-1;
+    }
+}
+System.out.println(ans);
+    }
+}
