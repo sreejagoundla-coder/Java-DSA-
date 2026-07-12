08d41fe48cc3d342e0f397705b192dfdb81c2146
sreeja <sreejagoundla@gmail.com>
2026-07-12 23:34:26 +0530

    Added Ship capacity by Binary Search Problem

---
+++ Arrays/ShipCapacity.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class ShipCapacity{
+public static int max(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+public static int sum(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
+}
+    public static void main(String[] args) {
+        int[] weights = {1,2,3,4,5,6,7,8,9,10};
+int left=max(weights);
+int right=sum(weights);
+int ans=Integer.MAX_VALUE;
+while(left<=right){
+    int mid=(left+right)/2;
+    int Load=0;
+    int Days=1;
+    int D=5;
+    for(int i=0;i<weights.length;i++){
+        if(Load+weights[i]<=mid){
+            Load+=weights[i];
+        }
+        else{
+            Days++;
+            Load=weights[i];
+        }
+    }
+    if(Days<=D){
+        ans=mid;
+        right=mid-1;
+    }
+    else{
+        left=mid+1;
+    }
+}
+System.out.println(ans);
+    }
+}
