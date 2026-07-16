509c426ca95857caa9477ad51bc88a6c268d9891
sreeja <sreejagoundla@gmail.com>
2026-07-16 23:20:15 +0530

    Added Lc 154 Find min in rotated sorted array

---
+++ Arrays/FindMin2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class FindMin2 {     // Lc 154. Find Minimum in Rotated Sorted Array II
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,0,1};
+        System.out.println(MinSorted(arr));
+    }
+
+    public static int MinSorted(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid]==arr[left] && arr[mid]==arr[right]){
+                left++;
+                right--;
+            } else if(arr[mid]>arr[right]){
+                left=mid+1;
+            } else {
+                right=mid;
+            }
+        }
+        return arr[left];
+    }
+}
