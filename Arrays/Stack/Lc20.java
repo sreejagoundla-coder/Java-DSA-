495f672dbe809f0175debe444b584aaaad969771
sreeja <sreejagoundla@gmail.com>
2026-08-08 23:11:28 +0530

    added 20

---
+++ Arrays/Stack/Lc20.java
@@ -0,0 +1,26 @@
+package Stack;
+import java.util.Stack;
+public class Lc20 {
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{'  || c=='['){
+                stack.push(c);
+            }
+        else{
+            if(stack.isEmpty()){
+                return false;
+            }
+            char top = stack.pop();
+            if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')){
+                return false;
+            }
+        }
+    }
+        return stack.isEmpty();
+}
+public static void main(String[] args){
+        String s = "({[]})";
+        System.out.println(isValid(s));
+    }
+}
